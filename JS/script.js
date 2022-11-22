a4f4bb1d2b4d43fba537c5ece46f4b9165943aaa
SimonePresti <prestisimone96@gmail.com>
2022-11-22 17:55:32 +0100

    Fixed js complete

--- JS/script.js
+++ JS/script.js
@@ -11,25 +11,28 @@ let ticketCost;
 
     let howManyKm = (0.2762 * inputDistanceElement.value);
 
-    console.log (`Quanti km: ${inputDistanceElement.value}`)
+    console.log (`Quanti km: ${inputDistanceElement.value}`);
 
-    console.log (`Quanti anni ha: ${inputAgeElement.value}`)
+    console.log (`Quanti anni ha: ${inputAgeElement.value}`);
 
     let discountMinors = ((howManyKm / 100) * 17.5);
     
     let discountMayors = ((howManyKm / 100) * 33.3);
     
-    let ticketCost = howManyKm
+    let ticketCost = howManyKm;
+    console.log (`Il Prezzo del biglietto senza sconto è di: ${ticketCost.toFixed(2)}€`);
 
 
 if (parseInt (inputAgeElement.value) < 18)  {
     ticketCost = (howManyKm - discountMinors);
-    console.log (`Prezzo biglietto scontato Minorenni è di: ${ticketCost.toFixed(2)}€`)
+    console.log (`Il Prezzo del biglietto scontato per i Minorenni è di: ${ticketCost.toFixed(2)}€`)
 
 } else if (parseInt (inputAgeElement.value) > 65)  {
     ticketCost = (howManyKm - discountMayors);
-    console.log (`Prezzo biglietto scontato Anziani è di: ${ticketCost.toFixed(2)}€`)
+    console.log (`Il Prezzo del biglietto scontato per gli Anziani è di: ${ticketCost.toFixed(2)}€`)
 } 
+
+
 
 output.innerHTML = `Il costo del biglietto è di: ${ticketCost.toFixed(2)}&euro;`;
  })
