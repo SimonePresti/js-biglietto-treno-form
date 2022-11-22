6c7537e2531dd3e72feb562c303e190bcee8ea99
SimonePresti <prestisimone96@gmail.com>
2022-11-22 17:51:52 +0100

    js fixed

--- JS/script.js
+++ JS/script.js
@@ -3,36 +3,33 @@ const inputDistanceElement = document.querySelector ('input#distance');
 const inputAgeElement = document.querySelector ('input#age');
 
 const button = document.querySelector ('div.buttons a.btn');
-const output = document.querySelector ('output');
+const output = document.getElementById ('output');
 
-const km = inputDistanceElement.value;
-
-console.log (`Quanti km: ${km}`)
-
-const age = inputAgeElement.value;
-
-console.log (`Quanti anni ha il cliente: ${age}`)
-
-let howManyKm = (0.2762 * km);
-
-console.log (`Il prezzo base del biglietto al km è di: ${howManyKm.toFixed(2)}€ `);
-
-let discountMinors = ((howManyKm / 100) * 17.5);
-
-let discountMayors = ((howManyKm / 100) * 33.3);
-
-let ticketCost = howManyKm
-
+let ticketCost;
 
  button.addEventListener ('click', function (){
 
-    output.innerHTML = `Il costo del biglietto è di: ${ticketCost}&euro;`;
+    let howManyKm = (0.2762 * inputDistanceElement.value);
 
-if (parseInt (km.value) < 18)  {
+    console.log (`Quanti km: ${inputDistanceElement.value}`)
+
+    console.log (`Quanti anni ha: ${inputAgeElement.value}`)
+
+    let discountMinors = ((howManyKm / 100) * 17.5);
+    
+    let discountMayors = ((howManyKm / 100) * 33.3);
+    
+    let ticketCost = howManyKm
+
+
+if (parseInt (inputAgeElement.value) < 18)  {
     ticketCost = (howManyKm - discountMinors);
+    console.log (`Prezzo biglietto scontato Minorenni è di: ${ticketCost.toFixed(2)}€`)
 
-} else if (parseInt (age.value) > 65)  {
+} else if (parseInt (inputAgeElement.value) > 65)  {
     ticketCost = (howManyKm - discountMayors);
-}
+    console.log (`Prezzo biglietto scontato Anziani è di: ${ticketCost.toFixed(2)}€`)
+} 
 
+output.innerHTML = `Il costo del biglietto è di: ${ticketCost.toFixed(2)}&euro;`;
  })
